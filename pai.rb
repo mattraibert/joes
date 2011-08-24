4cf0574df4bdc8af61cd280484197c4280c34595
Matt Raibert <mraibert@cyrusinnovation.com>
2011-08-24 16:51:43 -0400

    cleanup

--- pai.rb
+++ pai.rb
@@ -5,9 +5,6 @@ def get_password(prompt="Enter Password")
   ask(prompt) {|q| q.echo = false}
 end
 
-a = Mechanize.new
-
-
 def links(page)
   page.links.each do |link|
     text = link
@@ -15,18 +12,18 @@ def links(page)
   end
 end
 
-login_page = a.get("https://www.pai.com/employee/default.aspx")
+a = Mechanize.new
+page = a.get("https://www.pai.com/employee/default.aspx")
 
-my_page = login_page.form do |f| 
+page = page.form do |f| 
   f.txtUserName = "mattraibert"
-  #pw = get_password()
-  f.txtPassword = 'suiluj21'
+  f.txtPassword = get_password()
 end.click_button
 
-my_401k = a.click(my_page.link_with(:text => /My 401k/))
-statements = a.click(my_401k.link_with(:text => /Statements/))
+page = a.click(page.link_with(:text => /My 401k/))
+page = a.click(page.link_with(:text => /Statements/))
 
-stmt = statements.form do |f|
+stmt = page.form do |f|
   f.field("_ctl23:_ctl1:txtFrom").value = "1/1/2007"
   f.field("_ctl23:_ctl1:txtTo").value = "1/1/2010"
 end.click_button
