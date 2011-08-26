b58119d13e361656284e93318b8ae21fe6d6895e
Matt Raibert <mraibert@cyrusinnovation.com>
2011-08-26 11:18:37 -0400

    ask for username and start date. and iterate through all months for 4 years.

--- pai.rb
+++ pai.rb
@@ -1,32 +1,30 @@
 require 'mechanize'
 require 'highline/import'
-
-def get_password(prompt="Enter Password")
-  ask(prompt) {|q| q.echo = false}
-end
-
-def links(page)
-  page.links.each do |link|
-    text = link
-    puts text
-  end
-end
+require 'date'
+require 'active_support/core_ext'
 
 a = Mechanize.new
 page = a.get("https://www.pai.com/employee/default.aspx")
 
+months = (0..(4 * 12)).map { |month| Date.parse(ask("401k start date")) + month.months }
+
 page = page.form do |f| 
-  f.txtUserName = "mattraibert"
-  f.txtPassword = get_password()
+  f.txtUserName = ask("Enter Username")
+  f.txtPassword = ask("Enter Password") {|q| q.echo = false}
 end.click_button
 
 page = a.click(page.link_with(:text => /My 401k/))
 page = a.click(page.link_with(:text => /Statements/))
 
-stmt = page.form do |f|
-  f.radiobutton_with(:name => /_ctl23__ctl1_rdoManual/).check
-  f.field("_ctl23:_ctl1:txtFrom").value = "1/1/2007"
-  f.field("_ctl23:_ctl1:txtTo").value = "1/1/2010"
-end.click_button
-stmt.save
+months.each do |start_date|
+  file_name = "#{start_date + 1.month}.pdf"
 
+  stmt = page.form do |f|
+    f.radiobutton_with(:value => /rdoManual/).check
+    f.field("_ctl23:_ctl1:txtFrom").value = start_date.strftime("%-m/%-d/%Y")
+    f.field("_ctl23:_ctl1:txtTo").value = (start_date + 1.month).strftime("%-m/%-d/%Y")
+  end.click_button.save_as(file_name)
+
+  `pdftotext #{file_name}`
+  `rm #{file_name}`
+end
