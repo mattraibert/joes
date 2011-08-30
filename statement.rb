681c00b479e4afe1b3c0e8fa0b3edf21684e4aa8
Matt Raibert <mraibert@cyrusinnovation.com>
2011-08-29 20:46:34 -0400

    now mapped by fund and date

--- statement.rb
+++ statement.rb
@@ -1,19 +1,30 @@
 require 'date'
 
-files = Dir.glob("*-*-*.txt").map do |filename|
+class Hash
+  def hashmap
+    self.inject({}) do |newhash, (k,v)|
+      newhash[k] = yield(k, v)
+      newhash
+    end
+  end
+end
+
+files = {}
+
+Dir.glob("*-*-*.txt").each do |filename|
   file = []
   File.open(filename, "r") do |infile|
     while (line = infile.gets)
       file << line
     end
   end
-  [Date.parse(filename.sub(".txt","")), file]
+  files[Date.parse(filename.sub(".txt",""))] = file
 end
 
-data = files.map do |file|
+data = files.hashmap do |date, file|
   bal = 0
   interesting = []
-  file[1].each do |line|
+  file.each do |line|
     if bal > 0
       interesting << line
       bal += 1
@@ -23,26 +34,51 @@ data = files.map do |file|
       bal = 1
     end
   end
-  [file[0], interesting]
+  interesting
 end
 
-data.each do |file|
-  datum = file[1]
+class Array
+  def ziphash(values)
+    hash = {}
+    self.zip(values){|k,v| hash[k] = v}
+    hash
+  end
+end
+
+data = data.hashmap do |date, file|
+  datum = file
+  datum[2].strip
   datum[2].gsub!("iShares", "XXXiShares")
   datum[2].gsub!("Vanguard", "XXXVanguard")
   datum[2].gsub!("SPDR","XXXSPDR")
   datum[2].sub!("XXX","")
-  
-  
   funds = datum[2].split("XXX")
   funds << "TOTAL"
   balances = datum[0].split(" ")
-#  puts funds.join " | "
-#  puts balances
 
-  myhash = {}
-  funds.zip(balances){|fund,balance| myhash[fund] = balance}
-  puts file[0]
-  puts myhash
+  hash = {}
+  funds.zip(balances){|fund,balance| hash[fund.strip] = balance}
+  hash
 end
+
+fund_data = {}
+
+data.each do |d|
+  date = d[0]
+  balances = d[1]
+
+  balances.each do |b|
+    fund = b[0]
+    balance = b[1]
+    fund_data[fund] ||= {}
+    fund_data[fund][date.strftime("%-m/%-d/%Y")] = balance
+  end
+end
+
+fund_data.each do |f|
+  puts "#{f[0]} has data #{f[1]}\n\n" 
+end
+
+
+
 
