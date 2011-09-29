e7d0250089f21eab9d9afdccc7f1157fff383764
Matt Raibert <mattraibert@gmail.com>
2011-09-29 14:03:10 -0500

    move fund creation to investments

--- test/investments_test.rb
+++ test/investments_test.rb
@@ -5,28 +5,32 @@ require 'active_support/core_ext'
 
 class InvestmentsTest < MiniTest::Unit::TestCase
   def test_csv
+    rd = Investments.new
     expected_csv = ["Date, iShares Raibert Fund, ANOTHER Fund, TOTAL",
      "2008-01-01, 50000, 40000, 90000",
      "2008-01-02, 50001, 40001, 90002"].join("\n")
 
-    irf = Fund.new("iShares Raibert Fund")
+    irf = rd.fund("iShares Raibert Fund")
     irf.write_balance(Date.parse("2008-01-01"), 50000)
     irf.write_balance(Date.parse("2008-01-02"), 50001)
 
-    af = Fund.new("ANOTHER Fund")
+    af = rd.fund("ANOTHER Fund")
     af.write_balance(Date.parse("2008-01-01"), 40000)
     af.write_balance(Date.parse("2008-01-02"), 40001)
 
-    total = Fund.new("TOTAL")
+    total = rd.fund("TOTAL")
     total.write_balance(Date.parse("2008-01-01"), 90000)
     total.write_balance(Date.parse("2008-01-02"), 90002)
 
-    rd = Investments.new({
-      "iShares Raibert Fund" => irf,
-      "ANOTHER Fund" => af,
-      "TOTAL" => total
-    })
-
     assert_equal(expected_csv, rd.csv)
   end
+
+  def test_gives_you_the_same_fund
+    investments = Investments.new
+    
+    fund = investments.fund "Raibert Fund"
+    
+    assert_equal(fund, investments.fund("Raibert Fund"))
+  end
+
 end
